7ac5511624cfc7a5fea3bbfd567c2c6f1761a1c2
Paul Romeo <promeo98@proton.me>
2023-12-04 16:05:24 -0500

    -Added verbose flag (-v)

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -7,6 +7,7 @@ import argparse
 argParser = argparse.ArgumentParser(description='Organizes files in a directory into folders by extension',
                                     usage='%(prog)s [h] [-d DIR]')
 argParser.add_argument('-d', '--dir', help='directory to organize', default='.')
+argParser.add_argument('-v', '--verbose', help='toggle verbose output', action='store_true')
 args = argParser.parse_args()
 
 # file extensions to organize
@@ -52,20 +53,19 @@ extensions = {
 path = args.dir
 # Set verbose to 1 to show all file moves
 # Set verbose to 0 to show only basic info
-verbose = 1
 
 for extension, folder_name in extensions.items():
     # get all the files matching the extension in the dir
     files = glob.glob(os.path.join(path, f"*.{extension}"))
     print(f"[*] Found {len(files)} files with {extension} extension")
     if not os.path.isdir(os.path.join(path, folder_name)) and files:
-        # if the folder doesnt exist, create it
+        # if the folder doesn't exist, create it
         print(f"[+] Making {folder_name} folder")
         os.mkdir(os.path.join(path, folder_name))
     for file in files:
         # for each file with that extension move it to the folder
         basename = os.path.basename(file)
         dst = os.path.join(path, folder_name, basename)
-        if verbose:
+        if args.verbose:
             print(f"[*] Moving {file} to {dst}")
         shutil.move(file, dst)
