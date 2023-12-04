6e87707661e49ffdec67bff6c8883d982a5fcda2
Paul Romeo <promeo98@proton.me>
2023-12-04 15:34:57 -0500

    -Added arguments for directories using argv -Added a help argument

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import glob
 import shutil
 import argparse
 
-#parses command line arguments
-argParser = argparse.ArgumentParser(description='Organizes files in a directory into folders by extension')
+# parses command line arguments
+argParser = argparse.ArgumentParser(description='Organizes files in a directory into folders by extension',
+                                    usage='%(prog)s [h] [-d DIR]')
 argParser.add_argument('-d', '--dir', help='directory to organize', default='.')
 argParser.add_argument('-h', '--help', help='show this help')
 args = argParser.parse_args()
@@ -13,8 +14,7 @@ if args.help:
     print('Usage: python3 main.py -d <X:/path/to/directory/to/organize')
     exit()
 
-
-#file extensions to organize
+# file extensions to organize
 extensions = {
     'jpg': 'images',
     'png': 'images',
@@ -53,22 +53,22 @@ extensions = {
     'log': 'logs'
 }
 
-#calls back the directory supplied earlier
+# calls back the directory supplied earlier
 path = args.dir
-#Set verbose to 1 to show all file moves
-#Set verbose to 0 to show only basic info
+# Set verbose to 1 to show all file moves
+# Set verbose to 0 to show only basic info
 verbose = 1
 
 for extension, folder_name in extensions.items():
-    #get all the files matching the extension in the dir
+    # get all the files matching the extension in the dir
     files = glob.glob(os.path.join(path, f"*.{extension}"))
     print(f"[*] Found {len(files)} files with {extension} extension")
     if not os.path.isdir(os.path.join(path, folder_name)) and files:
-        #if the folder doesnt exist, create it
+        # if the folder doesnt exist, create it
         print(f"[+] Making {folder_name} folder")
         os.mkdir(os.path.join(path, folder_name))
     for file in files:
-        #for each file with that extension move it to the folder
+        # for each file with that extension move it to the folder
         basename = os.path.basename(file)
         dst = os.path.join(path, folder_name, basename)
         if verbose:
